9e8ca5a8de0048ed68b8c1dff0504d68d5a854d7
Rizky DPS <rzkydps@gmail.com>
2021-07-03 02:57:46 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<style>
+
+</style>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator PHP</title>
+</head>
+
+<body>
+    <div class="kalkulator">
+        <input type="text" id="layar">
+        <div class="container-button">
+            <span class="button">C</span>
+            <span class="button">
+                << /span>
+                    <span class="button">.</span>
+                    <span class="button">/</span>
+                    <span class="button">7</span>
+                    <span class="button">8</span>
+                    <span class="button">9</span>
+                    <span class="button">*</span>
+                    <span class="button">4</span>
+                    <span class="button">5</span>
+                    <span class="button">6</span>
+                    <span class="button">+</span>
+                    <span class="button">1</span>
+                    <span class="button">2</span>
+                    <span class="button">3</span>
+                    <span class="button">-</span>
+                    <span class="button">0</span>
+                    <span class="button">=</span>
+        </div>
+    </div>
+</body>
+
+</html>
